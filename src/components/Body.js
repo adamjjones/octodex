49376bbd268b45c3b3e5c6e2b1303a4d8ca859ed
Adam Jones <adam@linuxtampa.com>
2019-08-19 22:34:40 -0400

    Changed background color, but couldn't get the array of object to work. I kept getting a syntax error that I could not figure out how to fix.

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,10 +6,62 @@ import punkcat from './images/daftpunkcat.gif'
 import spider from './images/spidercat.png'
 import droid from './images/droidcat.png'
 
-export class body extends Component {
+const octocats = [
+  {
+    number: '#145',
+    name: 'Fire Fighter Octocat',
+    url: fire,
+    alt: 'Fire Fighter Octocat'
+  },
+  {
+    number: '#104',
+    name: 'Liberty Octocat',
+    url: fire,
+    alt: 'Liberty Octocat'
+  },
+  {
+    number: '#102',
+    name: 'DaftPunk Octocat',
+    url: fire,
+    alt: 'DaftPunkOctocat'
+  },
+  {
+    number: '#89',
+    name: 'Spider Octocat',
+    url: fire,
+    alt: 'Spider Octocat'
+  },
+  {
+    number: '#80',
+    name: 'Droid Octocat',
+    url: fire,
+    alt: 'Droid Octocat'
+  }
+]
+
+// class OctoCats extends Component {
+//   render() {
+//     return (
+//       <p>
+//         {octocats.map(character => {
+//           return (
+//             <Octocat
+//               number={octocats.number}
+//               name={octocats.name}
+//               url={octocats.url}
+//               alt={octocats.alt}
+//             />
+//           )
+//         })}
+//       </p>
+//     )
+//   }
+// }
+
+class body extends Component {
   render() {
     return (
-      <div>
+      <div className="body">
         <img className="catimages" src={sentry} />
         <img className="catimages" src={fire} />
         <img className="catimages" src={liberty} />
